5773100bc4eeabc598ff63caf5ddc877927129f0
Enkhtulga <gantsagaan_enkhtulga@unimedia.co.jp>
2021-08-23 19:42:18 +0800

    refactor podcast

--- src/components/AudioPlayerTransparent/index.js
+++ src/components/AudioPlayerTransparent/index.js
@@ -1,23 +1,48 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Button } from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
 import { Colors } from "../../theme/colors";
 import NCard from "../Card";
 import NAvatar from "../Avatar";
 import NCardContent from "../Card/CardContent";
 import "react-h5-audio-player/lib/styles.css";
+import { useRouter } from "next/router";
 
 const NAudioPlayerTransparent = ({ image, podcastName, rating }) => {
   const classes = useStyles();
+  const router = useRouter();
 
   return (
     <Box className={classes.wrap}>
       <NCard className={classes.shadowCardWrap} square>
         <NAvatar size={184} src={image || "/podcast_page_slider_img_1.jpg"} />
         <NCardContent className={classes.contentWrap}>
+          <Box display="flex">
+            <Typography variant="h1" className={classes.podcast}>
+              Шилдэг подкаст
+            </Typography>
+            <Rating
+              name="read-only"
+              value={rating || 5}
+              readOnly
+              size="small"
+              className={classes.rating}
+            />
+          </Box>
           <Typography variant="body1" className={classes.title}>
             {podcastName || "LOKI + Black Widow"}
           </Typography>
+          <Box width={170} maxWidth="100%">
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={() => router.push("/podcast/1")}
+            >
+              Сонсох
+            </Button>
+          </Box>
         </NCardContent>
       </NCard>
     </Box>
@@ -38,6 +63,7 @@ const useStyles = makeStyles((theme) => ({
   },
   wrap: {},
   contentWrap: {
+    paddingTop: theme.spacing(3),
     paddingRight: 0,
     paddingLeft: theme.spacing(2),
     width: "100%",
@@ -46,6 +72,18 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 48,
     lineHeight: "48px",
     color: Colors.white,
+    marginBottom: theme.spacing(2),
+  },
+  rating: {
+    marginLeft: theme.spacing(2),
+    fontSize: 14,
+    marginTop: 3,
+  },
+  podcast: {
+    fontSize: 18,
+    lineHeight: "20px",
+    color: Colors.white,
+    marginBottom: theme.spacing(1),
   },
   [theme.breakpoints.down("sm")]: {
     shadowCardWrap: { width: "auto" },
